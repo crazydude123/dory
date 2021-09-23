ea2fd336be9c318fdf21938f31fe901a00c49bae
vijaravi <vijaravi@cisco.com>
2021-09-23 11:31:20 +0530

    t

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -102,7 +102,7 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
 
     std::hash<std::string> mystdhash;
     int hashindex = ((static_cast<int>(mystdhash(keyy))) % kvlength);
-    std::cout << ((static_cast<int>(mystdhash(keyy))) << " " << hashindex << std::endl;
+    std::cout << (static_cast<int>(mystdhash(keyy)) << " " << hashindex << std::endl;
     /*
     for (int i = hashindex; i < kvlength + hashindex; i++) {
       if (kvstore[i % kvlength].key.empty() ||
