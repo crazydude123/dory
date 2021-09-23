f9768cfd1afc95e57738f96be1b2fa8e7e560713
vijaravi <vijaravi@cisco.com>
2021-09-23 16:45:43 +0530

    fvbvbb

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -156,10 +156,10 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
       if ((err = consensus.propose(&(payloads[i % 8192][0]), payload_size)) !=
           dory::ProposeError::NoError) {
         uint8_t* f = &(payloads[i % 8192][0]);
-        std::cout << f << std::endl;
+        /*std::cout << f << std::endl;
         for (int n = 0; n < 8192; n++) {
           std::cout << f[n] << std::endl;
-        }
+        }*/
         std::cout << "Proposal failed at index " << i << std::endl;
         i -= 1;
         switch (err) {
