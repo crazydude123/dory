2da1ce06d5046c9c92ed8c8c8f9447294a984e11
vijaravi <vijaravi@cisco.com>
2021-09-23 15:22:11 +0530

    s

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -93,7 +93,7 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
   consensus.commitHandler([&payload_size]([[maybe_unused]] bool leader,
                                           [[maybe_unused]] uint8_t* buf,
                                           [[maybe_unused]] size_t len) {
-    /*std::ostringstream convert;
+    std::ostringstream convert;
     for (int a = 0; a < payload_size; a++) {
       convert << static_cast<char>(buf[a]);
     }
@@ -101,6 +101,7 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
     std::string keyy = keyval.substr(0, keylength);
     std::cout << keyval << " " << keyy << std::endl;
 
+    /*
     std::hash<std::string> mystdhash;
     int hashindexx = static_cast<int>(mystdhash(keyy));
     int hashindex = (hashindexx % keylength + keylength) % keylength;
