7c23f1f04d683add7798641d9fc47fddbee1d5f3
vijaravi <vijaravi@cisco.com>
2021-09-23 10:57:18 +0530

    does this work?

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -92,12 +92,14 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
   consensus.commitHandler([&payload_size]([[maybe_unused]] bool leader,
                                           [[maybe_unused]] uint8_t* buf,
                                           [[maybe_unused]] size_t len) {
-    /*std::ostringstream convert;
+    std::ostringstream convert;
     for (int a = 0; a < payload_size; a++) {
       convert << static_cast<char>(buf[a]);
     }
     std::string keyval = convert.str();
     std::string keyy = keyval.substr(0, keylength);
+    std::cout << keyval << " " << keyy << std::endl;
+    /*
     std::hash<std::string> mystdhash;
     int hashindex = static_cast<int>(mystdhash(keyy)) % kvlength;
     for (int i = hashindex; i < kvlength + hashindex; i++) {
