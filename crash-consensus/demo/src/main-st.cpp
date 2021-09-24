c7485ad8b72cdc768d8e5eb40d34fec9c869f4cf
vijaravi <vijaravi@cisco.com>
2021-09-24 12:58:44 +0530

    jjjjjjkkkk

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]) {
 
 void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
                int outstanding_req, dory::ThreadBank threadBank) {
-  vector<long> latencies;
+  std::vector<long> latencies;
   dory::Consensus consensus(id, remote_ids, outstanding_req, threadBank);
   consensus.commitHandler([&payload_size]([[maybe_unused]] bool leader,
                                           [[maybe_unused]] uint8_t* buf,
