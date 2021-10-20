e7383478e261a8be3f1e3d6b0e87f2aa51f3e434
vijaravi <vijaravi@cisco.com>
2021-10-20 14:33:38 +0530

    Oh bb

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -173,9 +173,10 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
       // GET_TIMESTAMP(timestamps_start[i]);
       // Encode process doing the proposal
       dory::ProposeError err;
-      // std::cout << "Proposing " << i << std::endl;
+      std::cout << "Proposing " << i << std::endl;
       //GET_TIMESTAMP(start_latency);
       err = consensus.propose(&(payloads[i % 8192][0]), payload_size);
+      std:cout << "Oh boy" << std::endl;
       ////GET_TIMESTAMP(end_latency);
       latencies_start.push_back((start_latency));
       latencies_end.push_back((end_latency));
