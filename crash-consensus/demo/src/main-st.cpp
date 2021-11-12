ff5f918de6181ab4e5d6dcef9b41cab89e07cfda
vijaravi <vijaravi@cisco.com>
2021-11-12 00:03:01 -0500

    safilaslfknn

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -229,36 +229,43 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
     std::cout << "Replicated " << times << " commands of size " << payload_size
               << " bytes in " << ELAPSED_NSEC(start_meas, end_meas) << " ns"
               << std::endl;
-    std::ofstream dump;
+    std::ofstream dump, dump1, dump2;
     dump.open("dump-st-" + std::to_string(payload_size) + "-" +
               std::to_string(outstanding_req) + ".txt");
     int start_range = 0;
     TIMESTAMP_T last_received;
     GET_TIMESTAMP(last_received);
-    for (unsigned int i = 0; i < latencies_start.size(); i++) {
-      dump << ELAPSED_NSEC(latencies_start.at(i), latencies_end.at(i)) << "\n";
+    for (unsigned int i = 0; i < latencies_start.size(); ++i) {
+      dump << (latencies_start.at(i).tv_nsec +
+               latencies_start.at(i).tv_sec * 1000000000UL)
+           << " "
+           << (latencies_end.at(i).tv_nsec +
+               latencies_end.at(i).tv_sec * 1000000000UL)
+           << " " << ELAPSED_NSEC(latencies_start.at(i), latencies_end.at(i))
+           << "\n";
     }
     dump.close();
 
-    std::ofstream dump1;
-    dump1.open("dump-st-gg" + std::to_string(payload_size) + "-" +
+    dump1.open("dump-st-mu-" + std::to_string(payload_size) + "-" +
                std::to_string(outstanding_req) + ".txt");
-
-    start_range = 0;
-    GET_TIMESTAMP(last_received);
 
     for (size_t i = 0; i < timestamps_ranges.size(); i++) {
       auto [last_id, timestamp] = timestamps_ranges[i];
       for (int j = start_range; j < last_id; j++) {
+        std::cout << i << " " << j << std::endl;
         last_received = timestamp;
         // std::cout << start_range << " " << last_id << " " << std::endl;
-        dump << ELAPSED_NSEC(timestamps_start[j], timestamp) << "\n";
+        dump1 << (timestamps_start[j].tv_nsec +
+                  timestamps_start[j].tv_sec * 1000000000UL)
+              << " " << (timestamp.tv_nsec + timestamp.tv_sec * 1000000000UL)
+              << " " << ELAPSED_NSEC(timestamps_start[j], timestamp) << "\n";
       }
 
       if (start_range < last_id) {
         start_range = last_id;
       }
     }
+    dump1.close();
     exit(0);
   }
 }
