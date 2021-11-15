7de488cdd643397450490d4becdc7461e63f91a7
vijaravi <vijaravi@cisco.com>
2021-11-15 12:24:51 -0500

    fhdisin

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -106,7 +106,7 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
   TIMESTAMP_T start_latency, end_latency;
   dory::Consensus consensus(id, remote_ids, outstanding_req, threadBank);
   consensus.commitHandler([&payload_size, &end_latency, &latencies_end,
-                           &start_latency, &latencies_start, &chumma,
+                           &start_latency, &latencies_start, &chumma, &id,
                            &kvstore]([[maybe_unused]] bool leader,
                                      [[maybe_unused]] uint8_t* buf,
                                      [[maybe_unused]] size_t len) {
