a47539585b07eb7313e281b827f2857a6e33d907
vijaravi <vijaravi@cisco.com>
2021-11-24 23:22:15 -0500

    fddf

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -175,12 +175,6 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
   timesread = new_words.size();
   payloadread = keylength + valuelength;
   // print all words in new_words_uint8
-  std::cout << "Just " << new_words_uint8.size() << new_words.size()
-            << std::endl;
-  for (int i = 0; i < new_words_uint8.size(); i++) {
-    std::cout << "just " << reinterpret_cast<char*>(new_words_uint8[i])
-              << std::endl;
-  }
 
   // end of reading workload from file
 
